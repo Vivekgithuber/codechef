b763bda61e06ad501997d063f6a7ab2428624448
Vivekgithuber <98093405+Vivekgithuber@users.noreply.github.com>
2022-05-21 11:55:09 +0530

    Create PRIZEPOOL.cpp

---
+++ PRIZEPOOL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    while(T--){
+        int X,Y;
+        cin>>X;
+        cin>>Y;
+        cout<<10*X+90*Y<<endl;
+        
+    }
+	// your code goes here
+	return 0;
+}
